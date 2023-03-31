30c3f75174cb92627041c72c86f2e66d51bc2c9f
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-03-31 09:06:53 +0530

    application.properties configuration and required packages added

---
+++ Insurance_Management_System/src/main/java/com/example/Insurance_Management_System/InsuranceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Insurance_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Insurance_Management_System/src/test/java/com/example/Insurance_Management_System/InsuranceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Insurance_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
